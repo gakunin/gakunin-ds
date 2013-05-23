1ef5fbaa499201c103ce03b739a2f09ae7422b91
Yoshiaki KAWANO <kawano.yoshiaki@secioss.co.jp>
2013-05-23 06:02:46 +0000

    ドメイン名ヒントの空ドメイン名チェック対応
    git-svn-id: file:///var/www/svn/discovery/embedded-ds/trunk@133 931d3740-233f-4a17-a110-19ba41fa7497

--- incsearch/functions-incsearch.php
+++ incsearch/functions-incsearch.php
@@ -24,7 +24,7 @@ function getDomainNameFromURIHint_IncSearch($HintKeyList){
 				if (count($HintKeyList) == $useMduiHintMax) {
 					return $HintKeyList;
 				}
-				if (preg_match('/'.$domainhint.'$/', $clientHostname)){
+				if ((!empty($domainhint)) && (preg_match('/'.$domainhint.'$/', $hostname))){
 					if (!checkHintIDP($key, $HintKeyList)) {
 						array_push($HintKeyList, $key);
 					}
