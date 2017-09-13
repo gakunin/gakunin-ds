f8f08e0dbbd8f595e82353e482feaed8a8a3820f
Takeshi NISHIMURA <takeshi@nii.ac.jp>
2017-09-13 10:41:29 +0000

    DISCOVERY-147: Use Object.assign() if possible
    git-svn-id: file:///var/www/svn/discovery/embedded-ds/trunk@232 931d3740-233f-4a17-a110-19ba41fa7497

--- incsearch/custom-header-incsearch.php
+++ incsearch/custom-header-incsearch.php
@@ -149,6 +149,8 @@
 	
 	function clone(obj) {
 		if (null == obj || "object" != typeof obj) return obj;
+		if (typeof Object.assign === 'function')
+			return Object.assign({}, obj);
 		var copy = obj.constructor();
 		for (var attr in obj) {
 			if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
